5ab25bc716f7e1430e2737c793c4efac9b0850d4
Vova0199 <vova0143@gmail.com>
2016-11-19 20:17:11 +0200

    Second Commit
    Створив список кнопок на екранах, змінив фон

---
+++ app/src/main/java/com/example/i1/test_9/Tab_2.java
@@ -0,0 +1,20 @@
+package com.example.i1.test_9;
+
+/**
+ * Created by i1 on 13.11.2016.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab_2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/i1/test_9/Tab_3.java
@@ -0,0 +1,21 @@
+package com.example.i1.test_9;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by i1 on 13.11.2016.
+ */
+
+public class Tab_3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+
+        return rootView;
+    }
+}
